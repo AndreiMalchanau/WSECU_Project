0c700e8cf0180b9b84494ec23474fa3cfc0d1d3c
AndreiMalchanau <malchanau@gmail.com>
2020-06-17 22:16:19 -0400

    project

--- src/test/java/com/WSECU/Pages/LoginElements.java
+++ src/test/java/com/WSECU/Pages/LoginElements.java
@@ -3,7 +3,6 @@ package com.WSECU.Pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import com.WSECU.testbase.BaseClass;
 import com.WSECU.utils.CommonMethods;
 
@@ -12,9 +11,9 @@ public class LoginElements extends CommonMethods{
 	@FindBy(id = "digital-banking-username")
 	public WebElement signIn;
 	
-	
 	@FindBy(xpath = "//input[@class = 'button']")
 	public WebElement signInButton;
+	
 	
 	
 	public LoginElements(){
